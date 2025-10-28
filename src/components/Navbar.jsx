2631b70620e2f3a1cda48ccbc8339ffcdcd3b8f6
Shivam Singh <shivamvns8840@gmail.com>
2025-10-28 22:53:12 +0530

    fix: renamed assets folder for Vercel case sensitivity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import logo from "../Assets/logo.png";
+import logo from "../assets/logo.png";
 import { CgGitFork, CgFileDocument } from "react-icons/cg";
 import { ImBlog } from "react-icons/im";
 import {
